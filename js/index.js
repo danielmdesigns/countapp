f14eb46a64df5bbbf00fbfd19c91f93bf835a725
danielmdesigns <danielwmaldonado@gmail.com>
2016-04-17 15:51:16 -0500

    comment sections
    comment sections

--- js/index.js
+++ js/index.js
@@ -3,35 +3,37 @@
 //has no name, and runs automaticlly
 (function(){
   
-  //helps find more errors, easily
-  "use strict";
-  
-  var displayValue = "0";
-  
-  $(".btn").on("click touch", function(){
-    var btnValue = $(this).data("value");
-    if(btnValue === "="){
-	  displayValue = displayValue.replace(/×/g , "*");
-	  displayValue = displayValue.replace(/÷/g , "/");
-      updateDisplay(processAnswer(), true);
-    }else if(btnValue === "clear"){
-      updateDisplay("0", true);
-    }else{
-      if(btnValue === "+" ||
-         btnValue === "-" ||
-         btnValue === "×" ||
-         btnValue === "÷"){
-       if((displayValue[displayValue.length-1] === "+") ||
-         (displayValue[displayValue.length-1] === "-") ||
-         (displayValue[displayValue.length-1] === "×") ||
-         (displayValue[displayValue.length-1] === "÷")){
-        displayValue = displayValue.substr(0, displayValue.length-1);
-       }
-      }
-      updateDisplay(btnValue, false);
-    }
-  });
-  
+//helps find more errors, easily
+"use strict";
+
+var displayValue = "0";
+
+//CLICK && TOUCH
+$(".btn").on("click touch", function(){
+  var btnValue = $(this).data("value");
+  if(btnValue === "="){
+	displayValue = displayValue.replace(/×/g , "*");
+	displayValue = displayValue.replace(/÷/g , "/");
+	updateDisplay(processAnswer(), true);
+  }else if(btnValue === "clear"){
+	updateDisplay("0", true);
+  }else{
+	if(btnValue === "+" ||
+	   btnValue === "-" ||
+	   btnValue === "×" ||
+	   btnValue === "÷"){
+	 if((displayValue[displayValue.length-1] === "+") ||
+	   (displayValue[displayValue.length-1] === "-") ||
+	   (displayValue[displayValue.length-1] === "×") ||
+	   (displayValue[displayValue.length-1] === "÷")){
+	  displayValue = displayValue.substr(0, displayValue.length-1);
+	 }
+	}
+	updateDisplay(btnValue, false);
+  }
+});
+
+//KEYPAD
 $(document).keydown(function(event) {
   var displayValue = "0";
   var keycode = event.which;
@@ -84,43 +86,45 @@ $(document).keydown(function(event) {
   }
 });
 
-  var processAnswer = function(){
-    try{
-      var result = eval(displayValue);
-      return result;
-    }catch(e){
-      return "Error";
-      }
-  };
+//EVALUATE
+var processAnswer = function(){
+  try{
+	var result = eval(displayValue);
+	return result;
+  }catch(e){
+	return "Error";
+	}
+};
+
+//UPDATE DISPLAY
+var updateDisplay = function(value, overwrite){
+
+  displayValue = displayValue.toString();
   
-  var updateDisplay = function(value, overwrite){
- 
-    displayValue = displayValue.toString();
-    
-    if(overwrite === true){
-      displayValue = value;
-    }else{
-      if(displayValue === "0" || displayValue === "+" || displayValue === "×" || displayValue === "÷" || displayValue === "Error" || displayValue === "Infinity"){
-        displayValue = value;
-      }else{
-        displayValue += value;
-      }
-    }
-	/*CHECK DISPLAY LENGTH*/
-	if(displayValue.length >= 10 && displayValue.length <= 16){
-		$(".display").css("font-size", "2em");
-  	}else if(displayValue.length >= 16 && displayValue.length <= 30){
-		$(".display").css("font-size", "1em");
-	}else if(displayValue.length >= 30){
-		$(".btn").addClass("disable");
-	}else if(displayValue.length <= 10){
-		$(".display").css("font-size", "3.5em");
+  if(overwrite === true){
+	displayValue = value;
+  }else{
+	if(displayValue === "0" || displayValue === "+" || displayValue === "×" || displayValue === "÷" || displayValue === "Error" || displayValue === "Infinity"){
+	  displayValue = value;
 	}else{
-    $(".display").css("font-size", "3.5em");
+	  displayValue += value;
+	}
   }
-	/*INSERT*/
-    $(".display").text(displayValue);
-  };
+  /*CHECK DISPLAY LENGTH*/
+  if(displayValue.length >= 10 && displayValue.length <= 16){
+	  $(".display").css("font-size", "2em");
+  }else if(displayValue.length >= 16 && displayValue.length <= 30){
+	  $(".display").css("font-size", "1em");
+  }else if(displayValue.length >= 30){
+	  $(".btn").addClass("disable");
+  }else if(displayValue.length <= 10){
+	  $(".display").css("font-size", "3.5em");
+  }else{
+  $(".display").css("font-size", "3.5em");
+}
+  /*INSERT*/
+  $(".display").text(displayValue);
+};
 
 })();
 
